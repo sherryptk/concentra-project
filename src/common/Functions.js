dd3347888427fe7b2960bc897368f177d0191611
Sherry Elle <sherryelle@Sherrys-MacBook.local>
2019-01-22 19:04:34 -0500

    exercise 2 added regex to convert from camel case to sentence case

--- src/common/Functions.js
+++ src/common/Functions.js
@@ -2,7 +2,15 @@
 
 // Converts camel case string to a sentence case
 export const formatCamelCaseString = text => {
-  return text;
+  return text
+     // insert a space between lower & upper
+     .replace(/([a-z])([A-Z])/g, '$1 $2')
+     // insert a space between lower & number
+     .replace(/([a-z])([0-9])/g, '$1 $2')
+     // space before last upper in a sequence followed by lower
+     .replace(/\b([A-Z]+)([A-Z])([a-z])/, '$1 $2$3')
+     // uppercase the first character
+     .replace(/^./, function(str){ return str.toUpperCase(); })
 };
 
 // Handles page navigation
